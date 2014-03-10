851a352ccca2a56ea3b6efa81f29d4a161f9c40b
picnii <sompop@picnii.me>
2014-03-10 21:06:44 +0700

    init

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*var app = angular.module('route', [
+  'ngRoute'
+]);
+ 
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'MainCtrl'
+      }).
+      when('/bill', {
+        templateUrl: 'pages/bill.html',
+        controller: 'BillCtrl'
+      }).
+      when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'ContactCtrl'
+      }).
+      when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'AboutCtrl'
+      }).
+      when('/products', {
+        templateUrl: 'pages/products.html',
+        controller: 'ProductCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);*/
